b3a01b200510935f4ee268fa42024b6d117d0d81
wisayy <davidguzhavin@gmail.com>
2023-09-29 13:20:59 +0300

    first commit

---
+++ src/jktv22game/App.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jktv22game;
+
+import java.util.Scanner;
+import java.util.Random;
+/**
+ *
+ * @author pupil
+ */
+class App {
+    final Scanner scanner;
+    final Random random;
+
+    void run() {
+        
+    }
+}
